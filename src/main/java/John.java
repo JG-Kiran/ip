dd98340d1a5c14e1c30e57138c054258f21bd47e
jgkiran2003 <jgkiran2003@gmail.com>
2025-08-20 16:58:31 +0800

    Level-2

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class John {
@@ -6,13 +7,21 @@ public class John {
         System.out.println("How may I assist you?\n");
 
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
+
         while(true) {
             String line = sc.nextLine().trim();
             if (line.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (line.equalsIgnoreCase("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+            } else {
+                tasks.add(line);
+                System.out.println("added: " + line);
             }
-            System.out.println(line);
         }
         sc.close();
     }

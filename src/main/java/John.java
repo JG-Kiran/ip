7885af0ca3f1ed34ff0e82603fb78be4826eadb2
jgkiran2003 <jgkiran2003@gmail.com>
2025-08-20 16:48:09 +0800

    Implement Level-1

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class John {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello! I'm John\nHow may I assist you?\n");
+        System.out.println("Hello! I'm John\n");
+        System.out.println("How may I assist you?\n");
+
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String line = sc.nextLine().trim();
+            if (line.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(line);
+        }
+        sc.close();
     }
 }
